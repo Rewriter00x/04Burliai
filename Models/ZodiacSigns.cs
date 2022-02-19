d9dd53781acaea2de3f7bdb154c492deeb17bc3c
Rewriter <colossus00x@icloud.com>
2022-02-20 00:56:19 +0200

    Added west zodiac

--- Models/ZodiacSigns.cs
+++ Models/ZodiacSigns.cs
@@ -6,6 +6,23 @@ using System.Threading.Tasks;
 
 namespace _01Burliai.Models
 {
+
+    enum WestZodiac
+    {
+        Capricorn,
+        Aquarius,
+        Pisces,
+        Aries,
+        Taurus,
+        Gemini,
+        Cancer,
+        Leo,
+        Virgo,
+        Libra,
+        Scorpio,
+        Sagittarius,
+    }
+
     class ZodiacSigns
     {
         #region Fields
@@ -17,6 +34,54 @@ namespace _01Burliai.Models
         {
             get { return _date; }
             set { _date = value; }
+        }
+
+        public String WestZodiacSign
+        {
+            get
+            {
+                switch (_date?.Month)
+                {
+                    case 1:
+                        if (_date?.Day < 21) return WestZodiac.Capricorn.ToString();
+                        return WestZodiac.Aquarius.ToString();
+                    case 2:
+                        if (_date?.Day < 20) return WestZodiac.Aquarius.ToString();
+                        return WestZodiac.Pisces.ToString();
+                    case 3:
+                        if (_date?.Day < 21) return WestZodiac.Pisces.ToString();
+                        return WestZodiac.Aries.ToString();
+                    case 4:
+                        if (_date?.Day < 20) return WestZodiac.Aries.ToString();
+                        return WestZodiac.Taurus.ToString();
+                    case 5:
+                        if (_date?.Day < 21) return WestZodiac.Taurus.ToString();
+                        return WestZodiac.Gemini.ToString();
+                    case 6:
+                        if (_date?.Day < 22) return WestZodiac.Gemini.ToString();
+                        return WestZodiac.Cancer.ToString();
+                    case 7:
+                        if (_date?.Day < 24) return WestZodiac.Cancer.ToString();
+                        return WestZodiac.Leo.ToString();
+                    case 8:
+                        if (_date?.Day < 24) return WestZodiac.Leo.ToString();
+                        return WestZodiac.Virgo.ToString();
+                    case 9:
+                        if (_date?.Day < 23) return WestZodiac.Virgo.ToString();
+                        return WestZodiac.Libra.ToString();
+                    case 10:
+                        if (_date?.Day < 23) return WestZodiac.Libra.ToString();
+                        return WestZodiac.Scorpio.ToString();
+                    case 11:
+                        if (_date?.Day < 23) return WestZodiac.Scorpio.ToString();
+                        return WestZodiac.Sagittarius.ToString();
+                    case 12:
+                        if (_date?.Day < 21) return WestZodiac.Sagittarius.ToString();
+                        return WestZodiac.Capricorn.ToString();
+                    default:
+                        return "";
+                }
+            }
         }
         #endregion
     }
