afd605ed5a9a6a1554fe051842f9c212cf58df16
Rewriter <colossus00x@icloud.com>
2022-04-15 11:10:51 +0200

    Added PersonInput Added TextBoxWithCaption

---
+++ Tools/Controls/PersonInput.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace _01Burliai.Tools.Controls
+{
+    /// <summary>
+    /// Interaction logic for PersonInput.xaml
+    /// </summary>
+    public partial class PersonInput : UserControl
+    {
+        public PersonInput()
+        {
+            InitializeComponent();
+            DpBirthDate.DisplayDateEnd = DateTime.Today;
+            DpBirthDate.DisplayDateStart = new DateTime(DateTime.Today.Year - 135, DateTime.Today.Month, DateTime.Today.Day);
+        }
+    }
+}
---
+++ Tools/Controls/TextBoxWithCaption.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+
+namespace _01Burliai.Tools.Controls
+{
+    /// <summary>
+    /// Interaction logic for TextBoxWithCaption.xaml
+    /// </summary>
+    public partial class TextBoxWithCaption : UserControl
+    {
+
+        public string Caption
+        {
+            get { return TbCaption.Text; }
+            set { TbCaption.Text = value; }
+        }
+
+        public string Value
+        {
+            get { return TbBox.Text; }
+        }
+
+        public TextBoxWithCaption()
+        {
+            InitializeComponent();
+        }
+    }
+}
